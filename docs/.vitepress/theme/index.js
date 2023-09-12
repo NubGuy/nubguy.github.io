1e9952cbfade23946503433a4e492d466039af47
Yougraj <yougrajbora.developer@gmail.com>
2023-09-12 16:18:34 +0530

    giscus setup

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,30 +1,34 @@
-import DefaultTheme from 'vitepress/theme';
-import giscusTalk from 'vitepress-plugin-comment-with-giscus';
-import { useData, useRoute } from 'vitepress';
+import DefaultTheme from "vitepress/theme";
+import giscusTalk from "vitepress-plugin-comment-with-giscus";
+import { useData, useRoute } from "vitepress";
 
 export default {
-    ...DefaultTheme,
-    enhanceApp(ctx) {
-        DefaultTheme.enhanceApp(ctx);
+  ...DefaultTheme,
+  enhanceApp(ctx) {
+    DefaultTheme.enhanceApp(ctx);
+    // ...
+  },
+  setup() {
+    // Get frontmatter and route
+    const { frontmatter } = useData();
+    const route = useRoute();
+
+    // Obtain configuration from: https://giscus.app/
+    giscusTalk(
+      {
+        repo: "NubGuy/nubguy.github.io",
+        repoId: "R_kgDOKSf_nA",
+        category: "Announcements", // default: `General`
+        categoryId: "DIC_kwDOKSf_nM4CZP8V",
+        mapping: "url", // default: `pathname`
+        inputPosition: "bottom", // default: `top`
+        lang: "en", // default: `zh-CN`
         // ...
-    },
-    setup() {
-        // Get frontmatter and route
-        const { frontmatter } = useData();
-        const route = useRoute();
-        
-        // Obtain configuration from: https://giscus.app/
-        giscusTalk({
-            repo: 'CompetitiveExamNotes/NotesForAssamComepetitiveExam',
-            repoId: 'R_kgDOJ7Jx7A',
-            category: 'Announcements', // default: `General`
-            categoryId: 'DIC_kwDOJ7Jx7M4CX3Un',
-            mapping: 'url', // default: `pathname`
-            inputPosition: 'bottom', // default: `top`
-            lang: 'en', // default: `zh-CN`
-            // ...
-        }, {
-            frontmatter, route
-        });
-    }
+      },
+      {
+        frontmatter,
+        route,
+      },
+    );
+  },
 };
